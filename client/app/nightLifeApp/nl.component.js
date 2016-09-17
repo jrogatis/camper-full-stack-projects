7a1329e8b9f0b6933803f0cea5f1d983545be514
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-09-17 10:47:11 -0300

    layout nightliife done

--- client/app/nightLifeApp/nl.component.js
+++ client/app/nightLifeApp/nl.component.js
@@ -6,40 +6,58 @@ import routing from './nl.routes';
 //import angularAnimate from 'angular-animate';
 //import angularMaterial from 'angular-material';
 //import ModalService from '../../components/modal/modal.service';
-import { requestYelp }  from './yelp';
+import {
+  requestYelp
+}
+from './yelp';
 
 export class NLController {
   /*@ngInject*/
   constructor($http, $scope) {
     this.$http = $http;
     this.$scope = $scope;
-   // //this.Auth = Auth;
-     this.$scope.callbackCounter = 0;
+    // //this.Auth = Auth;
+    this.$scope.callbackCounter = 0;
 
   }
 
-  searchVenue () {
-    requestYelp( {
-      location: 'San Francisco',
+  CaptEnter(e) {
+    if (event.which === 13) {
+      this.searchVenue()
+    }
+  }
+
+  searchVenue() {
+    requestYelp({
+      location: this.searchTerms,
       sort: '2'
-      }, this.$scope.callbackCounter, (url, param) => {
-        this.$http.jsonp(url,{params: param})
-          .success(ret => {
-          this.AllVenues = ret.data;
-          this.$scope.callbackCounter++
-          console.log('ret da consulta',ret);
+    }, this.$scope.callbackCounter, (url, param) => {
+      this.$http.jsonp(url, {
+          params: param
         })
-          .catch(error =>{console.log(error)})
+        .success(ret => {
+          this.AllVenues = ret.businesses;
+
+          console.log('ret da consulta', this.AllVenues);
+        })
+        .catch(error => {
+          console.log(error)
+        })
     })
+    this.$scope.callbackCounter++
   }
 
 }
 
 export default angular.module('camperFullStackProjectsApp.nl', [ngRoute])
-  .config(routing)
+  .config(routing, function ($mdThemingProvider) {
+    $mdThemingProvider.theme('dark-grey').backgroundPalette('grey').dark();
+    $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+    $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+    $mdThemingProvider.theme('dark-blue').backgroundPalette('blue').dark();
+  })
   .component('nl', {
     template: require('./nl.main.pug'),
     controller: NLController
   })
   .name;
-

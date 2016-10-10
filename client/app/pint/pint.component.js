1f6735aed53726cf86f765f68a2c87dfed5d27a1
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-10-10 11:02:32 -0300

    only need now the placeholder

--- client/app/pint/pint.component.js
+++ client/app/pint/pint.component.js
@@ -26,129 +26,128 @@ export class PintController {
     this.socket = socket;
 
 
-     $scope.$on('$destroy', function() {
+    $scope.$on('$destroy', function () {
       socket.unsyncUpdates('pints');
     });
   }
 
   $onInit() {
-   this.loadImages();
+    this.loadImages();
   }
 
   loadSocket() {
     this.socket.unsyncUpdates('pint');
     this.socket.syncUpdates('pint', this.allPints, (event, item) => {
-      this.allPintsToShow = this.allPints.concat([]);
-      console.log('sockrt')
+      //this.allPintsToShow = this.allPints.concat([]);
+      console.log('socket')
       switch (event) {
       case 'deleted':
-          break;
-        case 'created':
-         this.loadUserInfoOnPint(item)
-          break;
-        case 'updated':
-          this.loadUserInfoOnPint(item)
-          break;
-        default:
-          console.log('no default', event);
-          break;
+        this.allPintsToShow = this.allPints.concat([]);
+        break;
+      case 'created':
+        this.loadUserInfoOnPint(item)
+        this.allPintsToShow = this.allPints.concat([]);
+        break;
+      case 'updated':
+        this.loadUserInfoOnPint(item)
+        break;
+      default:
+        console.log('no default', event);
+        break;
       }
     });
     return true;
   }
 
-  loadUserInfoOnPint(item){
-        const indexToAdd = _.findIndex(this.allPints,  pint => {
-            return pint._id.toString() === item._id
-          })
-        this.$http.get(`/api/users/userInfo/${item.ownerId}`)
-          .then(result => {
-           const info = result.data
-            if (info.provider === 'twitter') {
-              this.allPints[indexToAdd].userImage = info.twitter.profile_image_url_https;
-              this.allPints[indexToAdd].userName = info.name;
-            }
-          //this.angularGridInstance.gallery.refresh()
-        })
-
-
+  loadUserInfoOnPint(item) {
+    const indexToAdd = _.findIndex(this.allPints, pint => {
+      return pint._id.toString() === item._id;
+    })
+    this.$http.get(`/api/users/userInfo/${item.ownerId}`)
+      .then(result => {
+        const info = result.data;
+        if (info.provider === 'twitter') {
+          this.allPints[indexToAdd].userImage = info.twitter.profile_image_url_https;
+          this.allPints[indexToAdd].userName = info.name;
+        }
+      })
   }
 
 
   loadImages() {
-     this.$http.get('/api/pint/')
+    this.$http.get('/api/pint/')
       .then(results => {
-        this.allPints =  results.data;
+        this.allPints = results.data;
         this.loadSocket();
-        this.allPints.map( (pint, index) =>{
+        this.allPints.map((pint, index) => {
           this.$http.get(`/api/users/userInfo/${pint.ownerId}`)
             .then(result => {
-             const info = result.data
+              const info = result.data
               if (info.provider === 'twitter') {
                 pint.userImage = info.twitter.profile_image_url_https;
                 pint.userName = info.name;
               }
-          })
+            })
         })
-       this.allPintsToShow = this.allPints.concat([]);
+        this.allPintsToShow = this.allPints.concat([]);
       });
   }
 
   filterForUser(id) {
     return el => {
-         return el.ownerId === id
+      return el.ownerId === id;
     }
   }
 
-  toggleAllImages () {
-        this.allPintsToShow = this.allPints.concat([]);
-        this.angularGridInstance.gallery.refresh();
+  toggleAllImages() {
+    this.allPintsToShow = this.allPints.concat([]);
+    this.angularGridInstance.gallery.refresh();
   }
 
- filterFromId(index) {
-   console.log('no filter');
-   this.allPintsToShow = this.allPints.filter(this.filterForUser(this.allPintsToShow[index].ownerId));
- }
+  filterFromId(index) {
+    console.log('no filter');
+    this.allPintsToShow = this.allPints.filter(this.filterForUser(this.allPintsToShow[index].ownerId));
+  }
 
 
- filterByActualUser() {
-   this.allPintsToShow = this.allPints.filter(this.filterForUser(this.Auth.getCurrentUserSync()._id));
- }
+  filterByActualUser() {
+    this.allPintsToShow = this.allPints.filter(this.filterForUser(this.Auth.getCurrentUserSync()._id));
+  }
 
   isLoggedIn() {
     return this.Auth.isLoggedInSync() ? true : false;
   }
 
   addPicture() {
-    if(this.Auth.isLoggedInSync()) {
-    const picToAdd = {
-      ownerId: this.Auth.getCurrentUserSync()._id,
-      imgUrl: this.UrlToAdd,
-      desc: this.DescToAdd
-    };
-    this.$http.post('api/pint', picToAdd);
+    if (this.Auth.isLoggedInSync()) {
+      const picToAdd = {
+        ownerId: this.Auth.getCurrentUserSync()._id,
+        imgUrl: this.UrlToAdd,
+        desc: this.DescToAdd
+      };
+      this.$http.post('api/pint', picToAdd);
     } else {
-        this.Modal.needLogin();
+      this.Modal.needLogin();
     }
- }
+  }
 
   isOwner(index) {
-   return this.isLoggedIn() && this.allPintsToShow[index].ownerId ===  this.Auth.getCurrentUserSync()._id.toString();
+    return this.isLoggedIn() && this.allPintsToShow[index].ownerId === this.Auth.getCurrentUserSync()._id.toString();
   }
 
   vote(index) {
-    if (this.Auth.isLoggedInSync()) {
+    if(this.Auth.isLoggedInSync()) {
       //fist check if the user alredy vote for this
       const curUserVotesIndex = _.findIndex(this.allPintsToShow[index].likes, likes => {
-        return likes.userId === this.Auth.getCurrentUserSync()._id
+        return likes.userId === this.Auth.getCurrentUserSync()._id;
       })
-       const observer = jsonpatch.observe(this.allPints[index]);
-      if(curUserVotesIndex === -1 ) {
-        this.allPints[index].likes.push({
+      const observer = jsonpatch.observe(this.allPintsToShow[index]);
+      if(curUserVotesIndex === -1) {
+        this.allPintsToShow[index].likes.push({
           userId: this.Auth.getCurrentUserSync()._id
         });
       } else {
-        this.allPints[index].likes.splice(curUserVotesIndex,1);
+        this.allPintsToShow[index].likes.splice(curUserVotesIndex, 1);
       }
       var patches = jsonpatch.generate(observer);
       this.$http.patch(`/api/pint/${this.allPints[index]._id}`, patches);
@@ -158,8 +157,9 @@ export class PintController {
   }
 
   deletePint(index) {
-      this.$http.delete(`/api/pint/${this.allPints[index]._id}`)
-      }
+    this.$http.delete(`/api/pint/${this.allPintsToShow[index]._id}`)
+  }
+
 }
 
 
@@ -170,5 +170,3 @@ export default angular.module('camperFullStackProjectsApp.pint', [ngRoute, _Auth
     controller: PintController
   })
   .name
-
-

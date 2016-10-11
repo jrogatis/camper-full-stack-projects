d9adaffca155c6ca8944de0244532f91addd1b3b
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-10-10 21:05:19 -0300

    clean

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -1,15 +1,27 @@
 import angular from 'angular';
 const ngRoute = require('angular-route');
 import routing from './main.routes';
-
+import ngTouch from 'angular-touch';
+import jkAngularCarousel from 'angular-jk-carousel';
 export class MainController {
-/*@ngInject*/
+  /*@ngInject*/
+  constructor() {
+    this.images = [
+      {src: '/assets/images/nl.png'},
+      {src: '/assets/images/books.png'},
+      {src: '/assets/images/stock.png'},
+      {src: '/assets/images/voting.png'},
+      {src: '/assets/images/pint.png'}
+    ]
+  }
 }
 
-export default angular.module('camperFullStackProjectsApp.main', [])
+export default angular.module('camperFullStackProjectsApp.main', ['jkAngularCarousel'])
   .config(routing)
   .component('main', {
     template: require('./main.pug'),
     controller: MainController
   })
   .name;
+
+
